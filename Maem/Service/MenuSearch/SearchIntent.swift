0252877e97686aeb41a347bc2040c6f770e589f4
Geoleno <geoleno20@gmail.com>
2026-07-02 13:31:31 +0700

    (feat) add SearchIntent

---
+++ Maem/Service/MenuSearch/SearchIntent.swift
@@ -0,0 +1,19 @@
+import Foundation
+import FoundationModels
+
+@Generable
+struct SearchIntent {
+    var wantCarbs: [Carb]? = nil
+    var wantProteinHewani: [ProteinHewani]? = nil
+    var wantProteinNabati: [ProteinNabati]? = nil
+    var wantVeggies: [Veggie]? = nil
+    var avoidAllergens: [Allergen]? = nil
+    var maxBudget: Int? = nil
+    var mustNotSpicy: Bool? = nil
+    var requireHalal: Bool? = nil
+    var forKid: Bool? = nil
+    var mealCategory: MealCategory? = nil
+    var cookMethodPreference: CookMethod? = nil
+    var texturePreference: Texture? = nil
+    var preferHealthy: Bool? = nil
+}
