2efd94eb91273385bfe3de0094c72d336b189630
stepanusimnuel <nuel597@gmail.com>
2026-07-03 08:52:21 +0700

    Add AppColor

---
+++ Maem/App/AppColor.swift
@@ -0,0 +1,34 @@
+//
+//  AppColor.swift
+//  Maem
+//
+//  Created by Stepanus Imanuel on 03/07/26.
+//
+
+import SwiftUI
+
+enum AppColor {
+
+    static let blue100 = Color("Blue100")
+    static let blue300 = Color("Blue300")
+    static let blue500 = Color("Blue500")
+    static let blue700 = Color("Blue700")
+
+    static let red50 = Color("Red50")
+    static let red100 = Color("Red100")
+    static let red300 = Color("Red300")
+    static let red500 = Color("Red500")
+    static let red700 = Color("Red700")
+
+    static let neutralWhite = Color("NeutralWhite")
+    static let neutralBlack = Color("NeutralBlack")
+    static let neutralDarkGrey = Color("NeutralDarkGrey")
+    static let neutralSystemGrey = Color("NeutralSystemGrey")
+    static let neutralMedGrey = Color("NeutralMedGrey")
+    static let neutralLightGrey = Color("NeutralLightGrey")
+
+    static let greenLight = Color("GreenLight")
+    static let greenDark = Color("GreenDark")
+    static let systemRedLight = Color("RedLight")
+    static let systemRedDark = Color("RedDark")
+}
