46f3d3a95f625de48708a9c50abbf8a0f1454265
Geoleno <geoleno20@gmail.com>
2026-07-10 17:02:51 +0700

    Extend ModelEnums with HalalStatus, CookMethod, MealCategory, and missing vocabulary

--- Maem/Models/ModelEnums.swift
+++ Maem/Models/ModelEnums.swift
@@ -5,102 +5,116 @@
 //  Created by Stepanus Imanuel on 05/07/26.
 //
 
-
 import Foundation
 
+enum HalalStatus: String, CaseIterable, Codable {
+    case bersertifikat = "Bersertifikat"
+    case belumSertifikasi = "Belum Tersertifikasi"
+    case nonHalal = "Non-Halal"
+}
+
 enum Allergen: String, CaseIterable, Codable {
-
     case peanut = "Kacang"
-
     case milk = "Susu"
-
     case shrimp = "Udang"
-    
     case shellfish = "Kerang"
-    
     case fish = "Ikan"
-
     case egg = "Telur"
-
     case gluten = "Gluten"
-    
     case sesame = "Wijen"
-
+    case soy = "Kedelai"
 }
 
-enum Carb: String, Codable {
-
-    case rice
-    case noodle
-    case bread
-    case potato
-    case porridge
-    case none
-
+enum Carb: String, CaseIterable, Codable {
+    case rice = "Nasi"
+    case noodle = "Mie"
+    case bread = "Roti"
+    case potato = "Kentang"
+    case friedRice = "Nasi Goreng"
+    case vermicelli = "Bihun"
+    case flatNoodle = "Kwetiau"
+    case glutinousRiceCake = "Lontong Ketupat"
+    case porridge = "Bubur"
+    case pasta = "Pasta"
+    case corn = "Jagung"
+    case cassava = "Singkong"
+    case none = "Tidak Ada"
 }
 
-enum AnimalProtein: String, Codable {
-
-    case chicken
-
-    case beef
-
-    case fish
-
-    case shrimp
-
-    case egg
-
+enum AnimalProtein: String, CaseIterable, Codable {
+    case chicken = "Ayam"
+    case beef = "Sapi"
+    case fish = "Ikan"
+    case shrimp = "Udang"
+    case egg = "Telur"
+    case goat = "Kambing"
+    case duck = "Bebek"
+    case meatball = "Bakso"
+    case sausage = "Sosis"
 }
 
-enum PlantProtein: String, Codable {
-
-    case tofu
-
-    case tempeh
-
-    case mushroom
-
-    case bean
-
+enum PlantProtein: String, CaseIterable, Codable {
+    case tofu = "Tahu"
+    case tempeh = "Tempe"
+    case mushroom = "Jamur"
+    case bean = "Kacang"
+    case edamame = "Edamame"
 }
 
-enum Portion: String, Codable {
-    case kids
-    
-    case reguler
-    
-    case sharing
+enum Portion: String, CaseIterable, Codable {
+    case kids = "Anak"
+    case reguler = "Reguler"
+    case sharing = "Sharing"
 }
 
-enum Texture: String, Codable {
-    case soupy
-    
-    case soft
-    
-    case crispy
-    
-    case dry
+enum Texture: String, CaseIterable, Codable {
+    case soupy = "Berkuah"
+    case soft = "Lembut"
+    case crispy = "Renyah"
+    case dry = "Kering"
+    case sticky = "Lengket"
 }
 
-enum Veggie: String, Codable {
-    case soup
-    
-    case spinach
-    
-    case tomato
-    
-    case carrot
+enum Veggie: String, CaseIterable, Codable {
+    case soup = "Sop"
+    case spinach = "Bayam"
+    case tomato = "Tomat"
+    case carrot = "Wortel"
+    case waterSpinach = "Kangkung"
+    case greenBeans = "Buncis"
+    case beanSprout = "Toge"
+    case broccoli = "Brokoli"
+    case eggplant = "Terong"
+    case mustardGreens = "Sawi"
+    case rawVeggiePlate = "Lalapan"
+    case mushroom = "Jamur"
 }
 
-enum Topping: String, Codable {
-    case tomato_sauce
-    
-    case chili_sauce
-    
-    case crackers
-    
-    case soy_sauce
-    
-    case chili_oil
+enum Topping: String, CaseIterable, Codable {
+    case tomato_sauce = "Saus Tomat"
+    case chili_sauce = "Sambal"
+    case crackers = "Kerupuk"
+    case soy_sauce = "Kecap"
+    case chili_oil = "Minyak Cabai"
+    case pickle = "Acar"
+    case friedEgg = "Telur Ceplok"
+    case cheese = "Keju"
+    case friedShallot = "Bawang Goreng"
+}
+
+enum CookMethod: String, CaseIterable, Codable {
+    case fried = "Goreng"
+    case grilled = "Bakar"
+    case roasted = "Panggang"
+    case steamed = "Kukus"
+    case boiled = "Rebus"
+    case stirFried = "Tumis"
+    case rawSalad = "Mentah Salad"
+}
+
+enum MealCategory: String, CaseIterable, Codable {
+    case heavyMeal = "Makanan Berat"
+    case snack = "Cemilan"
+    case drink = "Minuman"
+    case dessert = "Dessert"
 }
